15e4f0e57c02ba21ff373f9c494f7a052856f0d8
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-09-16 14:29:08 +0300

    1 - install flask package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,10 @@
 # install Flask python package
 
-exec { 'apt-get update':
-  command => '/usr/bin/apt-get update'
+exec { 'install flask':
+  command => 'pip3 install flask==2.1.0'
 }
 
 package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Exec['apt-get update']
+  ensure  => 'installed',
+  require => Exec['install flask']
 }
