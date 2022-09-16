ed753f6d7396247b6c834c8c86b86d41b511d5a2
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-09-16 14:20:45 +0300

    1 - ap-get update before installing flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# Install Flask python package
+# install Flask python package
+
+exec { 'apt-get update':
+ command => '/usr/bin/apt-get update'
+}
 
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Exec['apt-get update']
 }
