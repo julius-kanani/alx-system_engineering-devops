03940afcd90305f922a7fd959cf332ed24422c40
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-09-16 13:59:37 +0300

    0 - Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,13 @@
+$folder = '/tmp'
+
+file { $folder:
+  ensure => 'directory'
+}
+
+file { '/tmp/school':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+  require => File[$folder]
+}
