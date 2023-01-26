b7515806811b0cff02025aa061abe2bf26d7ef1a
JULIUS MOMANYI KANANI <99327314+julius-kanani@users.noreply.github.com>
2023-01-26 12:21:30 +0300

    sample

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+""""
+A Python script that, using a REST API, for a given employee ID, returns 
+information about his/her TODO list progress.
+"""
+
+
+import requests
+import sys
+
+
+def gather_user_data(emp_id):
+    """
+    Returns todo list information for a given employee.
+    """
+
+    employees = requests.get('https://jsonplaceholder.typicode.com/users')
+    todos_list = requests.get('https://jsonplaceholder.typicode.com/todos')
+
+    # Retrieve user name with given employee id
+    emp_name = None
+    for employee in employees:
+        if employee.get("emp_id") == emp_id:
+            emp_name = employee.get("name")
+            print(emp_name)
+
+
+if __name__ == "__main__":
+    employee_id = sys.argv[2]
+    gather_user_data(employee_id)
