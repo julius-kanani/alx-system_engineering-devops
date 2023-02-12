d05a06b82613118dfbdb9e053474eac3e50d6a57
Julius Kanani <juliusm.kanani@gmail.com>
2023-02-12 18:16:16 +0300

    2 - Recurse it.

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Recursive function that queries the REDDIT API and returns a list containing
+the titles of all hot articles for a given subreddit.
+"""
+
+
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    """ Returns a list containing the titles of all hot articles
+        for a given subreddit, if no results are found for the
+        given subreddit, the function returns None.
+    """
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+
+    # User-Agent to avoid being blocked by the Reddit API
+    headers = {"User-Agent": "RedditAPI Client"}
+
+    # GET request to the REDDIT API to retrieve information about
+    # hot articles in a given subreddit.
+    response = requests.get(url, headers)
+    if response.status_code == 404:
+        # if subreddit does not exist
+        return None
+
+    # parse the JSON data into a Python dictionary
+    json_data = response.json()
+
+    # Extract the list of hot articles from the JSON data returned
+    hot_posts = json_data['data']['children']
+
+    # Obtain title of every single hot article to the hot_list list.
+    for post in hot_posts:
+        host_list.append(post["data"]["title"])
+
+    after = data["data"]["after"]
+
+    if after is None:
+        return hot_list
+
+    return recurse(subreddit, hot_list)
