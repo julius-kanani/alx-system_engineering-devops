b34f9995809d5005fd93a4e9055d062c9a2d17ed
Julius Kanani <juliusm.kanani@gmail.com>
2024-03-22 21:52:32 +0300

    install flask package.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
 # install flask using puppet
 
-package { 'puppet-flask':
-    ensure   => '2.1.0',
-    name     => 'flask',
-    provider => 'pip3',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+    command     => '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.1.1',
+    environment => ['PATH=/usr/bin:/usr/local/bin'],
+    unless      => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
