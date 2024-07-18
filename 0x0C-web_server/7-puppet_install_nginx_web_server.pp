209b678f4028e97577809e15ed8102f67b84f727
julius-kanani <juliusm.kanani@gmail.com>
2024-07-18 14:49:18 +0000

    updated the puppet manifest file to setup nginx.

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,80 @@
-# Setup nginx server
-
-package { 'nginx':
-  ensure     => 'installed',
+# Install Nginx package
+package {'nginx':
+  ensure => installed,
 }
 
-file { '/var/www/html/index.nginx-debian.html':
+# Enable and start Nginx service
+service {'nginx':
+  ensure => running,
+  enable => true,
+}
+
+# Adjust the firewall to allow access to the Nginx service
+exec {'ufw allow Nginx HTTP':
+  command => 'ufw allow Nginx HTTP',
+  path    => '/usr/sbin:/usr/bin:/sbin:/bin',
+  unless  => 'ufw status | grep -q "Nginx HTTP"',
+}
+
+# Enable ufw if inactive
+exec {'ufw enable':
+  command => 'ufw enable',
+  path    => '/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => 'ufw status | grep -q "Status: inactive"',
+}
+
+# Create a new directory for the caesarus.tech website and set the appropriate permissions
+file {'/var/www/caesarus.tech/html':
+  ensure => directory,
+  owner  => $::puppetdb::clientcert,
+  group  => $::puppetdb::clientcert,
+  mode   => '0755',
+}
+
+# Create an index.html file for the caesarus.tech website
+file {'/var/www/caesarus.tech/html/index.html':
+  ensure => file,
+  owner  => $::puppetdb::clientcert,
+  group  => $::puppetdb::clientcert,
+  mode   => '0644',
   content => 'Hello World!',
 }
 
-file_line { 'redirect_me':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+# Create a new configuration file for the caesarus.tech server block
+file {'/etc/nginx/sites-available/caesarus.tech':
+  ensure => file,
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0644',
+  content => "server {\n    listen 80;\n    listen [::]:80;\n\n    root /var/www/caesarus.tech/html;\n    index index.html;\n\n    server_name caesarus.tech 127.0.0.1 localhost 18.210.15.46;\n\n    location / {\n        try_files \$uri \$uri/ =404;\n    }\n\n    location /redirect_me {\n        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n    }\n\n    error_page 404 /custom_404_Page.html;\n}\n",
 }
 
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+# Setup custom error 404 page.
+file {'/var/www/caesarus.tech/html/custom_404_Page.html':
+  ensure => file,
+  owner  => $::puppetdb::clientcert,
+  group  => $::puppetdb::clientcert,
+  mode   => '0644',
+  content => "Ceci n'est pas une page",
+}
+
+# Create a symbolic link to enable the caesarus.tech server block configuration
+file {'/etc/nginx/sites-enabled/caesarus.tech':
+  ensure => link,
+  target => '/etc/nginx/sites-available/caesarus.tech',
+}
+
+# Test the Nginx configuration
+exec {'nginx -t':
+  command => '/usr/sbin/nginx -t',
+  path    => '/usr/sbin:/usr/bin:/sbin:/bin',
+  require => File['/etc/nginx/sites-available/caesarus.tech'],
+  unless  => '/usr/sbin/nginx -t | grep -q "syntax is ok"',
+}
+
+# If the configuration is valid, restart Nginx
+Exec {'service nginx restart':
+  command => '/usr/sbin/service nginx restart',
+  path    => '/usr/sbin:/usr/bin:/sbin:/bin',
+  require => Exec['nginx -t'],
 }
